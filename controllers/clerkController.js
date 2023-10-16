c86df9f29c10bb7a7958d1c81b9e411ff304d04d
hxnshaw <eghosasere07@gmail.com>
2023-10-16 12:25:33 +0100

    updates for clerk

--- controllers/clerkController.js
+++ controllers/clerkController.js
@@ -45,7 +45,7 @@ exports.loginClerk = async (req, res) => {
 
     const user = await Clerk.findOne({ where: { email: email } });
     if (!user) {
-      res.status(404).json({ message: "Clerk Does Not Exist" });
+      return res.status(404).json({ message: "Clerk Does Not Exist" });
     }
     const passwordIsCorrect = await user.comparePassword(password);
     if (!passwordIsCorrect) {
@@ -80,6 +80,49 @@ exports.getAllClerks = async (req, res) => {
       return res.status(404).json({ message: "Clerks not found" });
     }
     res.status(200).json({ count: clerks.length, data: clerks });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateClerkProfile = async (req, res) => {
+  const { first_name, last_name, email, telephone_number } = req.body;
+
+  try {
+    if (!first_name || !last_name || !email || !telephone_number) {
+      throw new Error("Please Provide Valid Credentials");
+    }
+    const user = await Clerk.findOne({ where: { id: req.user.userId } });
+    if (!user) {
+      return res.status(404).json({ message: "clerk not found" });
+    }
+    user.first_name = first_name;
+    user.last_name = last_name;
+    user.email = email;
+    user.telephone_number = telephone_number;
+
+    await user.save();
+    const tokenUser = createTokenUser(user);
+    attachCookiesToResponse({ res, user: tokenUser });
+    res.status(200).json({ message: "Success", tokenUser });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateClerkPassword = async (req, res) => {
+  const { currentPassword, newPassword } = req.body;
+  try {
+    if (!currentPassword || !newPassword) {
+      throw new Error("Please Provide Valid Credentials");
+    }
+    const user = await Clerk.findOne({ where: { id: req.user.userId } });
+    if (!user) {
+      return res.status(404).json({ message: "Clerk Does Not Exist" });
+    }
+    user.set({ password: newPassword });
+    await user.save();
+    res.status(200).json({ message: "New Password Saved!" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- routes/clerkRouter.js
+++ routes/clerkRouter.js
@@ -6,16 +6,28 @@ const {
   loginClerk,
   logoutClerk,
   getAllClerks,
+  updateClerkProfile,
+  updateClerkPassword,
 } = require("../controllers/clerkController");
 
-router.route("/login").post(loginClerk);
+const { authenticateUser } = require("../middlewares/authentication");
 
-router.route("/profile/logout").get(logoutClerk);
-
-router.route("/").get(getAllClerks);
+router.route("/").get(authenticateUser, getAllClerks);
 
 router.route("/register").post(createClerk);
 
-router.route("/:email").get(getSingleClerk);
+router.route("/login").post(loginClerk);
+
+router.route("/profile/logout").get(authenticateUser, logoutClerk);
+
+router
+  .route("/profile/update-profile")
+  .put(authenticateUser, updateClerkProfile);
+
+router
+  .route("/profile/update-password")
+  .put(authenticateUser, updateClerkPassword);
+
+router.route("/:email").get(authenticateUser, getSingleClerk);
 
 module.exports = router;
