965a9e70ca6a1c4e0f4156c7f471f2fe2adf3dfd
hxnshaw <eghosasere07@gmail.com>
2024-08-08 16:58:03 +0100

    fix

--- config/config.js
+++ config/config.js
@@ -17,11 +17,12 @@ let config = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
   },
 };
 
