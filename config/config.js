63ae1245fc723fc4b41ed368fba36bbf46bfe5a1
hxnshaw <eghosasere07@gmail.com>
2024-08-08 16:49:45 +0100

    database config

---
+++ config/config.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+let config = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
+
+module.exports = config;
