5f8feb79ba683b8b366733b2598046f4245c716d
hxnshaw <eghosasere07@gmail.com>
2024-08-12 15:50:51 +0100

    ssl fixed

--- config/config.js
+++ config/config.js
@@ -2,27 +2,46 @@ require("dotenv").config();
 
 let config = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
-  production: {
-    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_NAME,
-    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        required: true,
+        rejectUnauthorized: false,
+      },
+    },
+
+    test: {
+      username: "root",
+      password: null,
+      database: "database_test",
+      host: "127.0.0.1",
+      dialect: "mysql",
+      dialectOptions: {
+        ssl: {
+          required: true,
+          rejectUnauthorized: false,
+        },
+      },
+    },
+    production: {
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE_NAME,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          required: true,
+          rejectUnauthorized: false,
+        },
+      },
+    },
   },
 };
 
